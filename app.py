db5fd4d314bc5d7b52da4205cfb239e825c110e4
Christina Chen <christinac@users.noreply.huggingface.co>
2022-11-29 21:30:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import gradio as gr
 from gradio.mix import Series
 
 #draw an input image based off of user's text input
-print("does this work" + os.listdir) 
+print("test")
+print(os.listdir) 
 
 def drawImage(text, font): #add another argument for prompt later
     out = Image.new("RGB", (512, 512), (0, 0, 0))

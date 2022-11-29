b9242144caf3b7417c486486f83afab0173e9c87
Christina Chen <christinac@users.noreply.huggingface.co>
2022-11-29 22:32:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ def drawImage(text, font): #add another argument for prompt later
     out = Image.new("RGB", (512, 512), (0, 0, 0))
     #move font to font-directory 
     #print(os.listdir())
-    print(font)
+    print('test')
+    print (font)
     print (os.path.abspath(font))
     #shutil.move(srcFolder+font,'font-directory')
     

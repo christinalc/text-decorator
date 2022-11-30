d40b2a7336f7322d24d52cba8d09b079aedcb2df
Christina Chen <christinac@users.noreply.huggingface.co>
2022-11-30 15:00:47 +0000

    Update app.py

--- app.py
+++ app.py
@@ -16,18 +16,18 @@ pipe.to(device)
 
 #draw an image based off of user's text input
 
-def drawImage(text, prompt): #(text, font)
+def drawImage(text, textsize, prompt, strength, guidance_scale): #(text, font)
     out = Image.new("RGB", (512, 512), (0, 0, 0))
     #add some code here to move font to font-directory   
     font = './font-directory/DimpleSans-Regular.otf'
-    fnt = ImageFont.truetype(font, 240)
+    fnt = ImageFont.truetype(font, textsize)
     d = ImageDraw.Draw(out)
     d.multiline_text((16, 64), text, font=fnt, fill=(255, 255, 255))
 
     #init_image = out
     out.save('initImage.png')
     images = []
-    images = pipe(prompt=prompt, init_image=out, strength=0.7, guidance_scale=7).images
+    images = pipe(prompt=prompt, init_image=out, strength=strength, guidance_scale=guidance_scale).images
     #images[0].save = ("image.png")
     #images = []
     #images.append(out)
@@ -50,7 +50,10 @@ demo = gr.Interface(
     inputs=[
         gr.Textbox(placeholder="shift + enter for new line",label="what do you want to say?"),
         #"file"
-        gr.Textbox(placeholder="prompt",label="how does your message look and feel?") #figure out models in series 
+        gr.Number(value=240),
+        gr.Textbox(placeholder="enter prompt",label="how does your message look and feel?"), #figure out models in series 
+        gr.Slider(label="strength", placeholder="controls the amount of noise that is added to the input image",minimum=0, maximum=1, step=0.01, value=0.7),
+        gr.Slider(label="guidance scale", placeholder="controls how much the image generation process follows the text prompt",value=7.5, maximum=20),
         ],
     outputs="image")
 demo.launch()

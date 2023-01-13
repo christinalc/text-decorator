28e335a89aa92d806d24c0288c0cf32107bf7eb9
Christina Chen <christinac@users.noreply.huggingface.co>
2023-01-13 22:25:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -46,8 +46,9 @@ def drawImage(text, text_size, prompt, strength, guidance_scale): #(text, text_s
 #blocks = gr.Blocks()
 
 demo = gr.Interface(
-    title="Try out the tool I built for AI Alphabet",
-    description="Save or screenshot your creations and share on https://forms.gle/qhzc7nfX7VGwBco96 ⚡️ (Note: I've upgraded the hardware today from 7-9pm so that it runs faster, if you visit this link in the future, it will be slower.)",
+    title="Text Decorator",
+    description="Note: This will be very slow since it is running on CPU.",
+    #description="Save or screenshot your creations and share on https://forms.gle/qhzc7nfX7VGwBco96 ⚡️ (Note: I've upgraded the hardware today from 7-9pm so that it runs faster, if you visit this link in the future, it will be slower.)",
     ##theme='huggingface',
     #css="""
     #body {font-family: system-ui, Helvetica, Arial, sans-serif}
